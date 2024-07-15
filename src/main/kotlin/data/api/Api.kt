8700b6821ccf08ec7ddb3d141197b9e342815a3e
mehdinajibpour <najibpour6@gmail.com>
2024-07-16 01:28:39 +0330

    Defined Api object with base URL and API key constants.

---
+++ src/main/kotlin/data/api/Api.kt
@@ -0,0 +1,8 @@
+package data.api
+
+object Api {
+    const val BASE_URL = "https://api.weatherapi.com/v1/current.json"
+    const val API_KEY = "570ca0810a9347a58b073703241407"
+    const val DEFAULT_FIELDS = "q"
+
+}
