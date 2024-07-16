43e198fb849376e3df0092cc4ad029caa16088b3
mehdinajibpour <najibpour6@gmail.com>
2024-07-16 22:27:27 +0330

    Comments attributes in WeatherInfo and adds DI to AppModule

--- src/main/kotlin/di/AppModule.kt
+++ src/main/kotlin/di/AppModule.kt
@@ -4,8 +4,11 @@ import data.api.WeatherService
 import data.api.WeatherServiceImpl
 import data.repository.WeatherRepository
 import org.koin.dsl.module
+import storegs.FileHistoryStorage
+import storegs.HistoryStorage
 
 val appModule = module {
     single<WeatherService> { WeatherServiceImpl() }
     single<WeatherRepository> { WeatherRepository(get()) }
+    single<HistoryStorage> { FileHistoryStorage("search_history.json") }
 }
--- src/main/kotlin/models/WeatherInfo.kt
+++ src/main/kotlin/models/WeatherInfo.kt
@@ -1,6 +1,9 @@
 package models
 
 import kotlinx.serialization.Serializable
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @Serializable
 data class WeatherInfo(
@@ -11,24 +14,25 @@ data class WeatherInfo(
 @Serializable
 data class Location(
     val name: String,
+    val request_time: String = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm").format(LocalDateTime.now())
 //    val region: String,
 //    val country: String,
-    val lat: Double,
-    val lon: Double,
+//    val lat: Double,
+//    val lon: Double,
 //    val tz_id: String,
 //    val localtime_epoch: Long,
-    val localtime: String
+//    val localtime: String
 )
 
 @Serializable
 data class CurrentWeather(
 //    val last_updated_epoch: Long,
-    val last_updated: String,
-    val temp_c: Double,
-    val temp_f: Double,
+//    val last_updated: String,
+//    val temp_c: Double,
+//    val temp_f: Double,
 //    val is_day: Int,
     val condition: WeatherCondition,
-    val wind_mph: Double,
+//    val wind_mph: Double,
 //    val wind_kph: Double,
 //    val wind_degree: Int,
 //    val wind_dir: String,
